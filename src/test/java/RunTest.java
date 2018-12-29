5264cc2751fbdd249c6cbf7b0c78266f58af8615
Anton Hromov <svsruof@gmail.com>
2018-12-29 14:47:33 +0300

    Гитигнор, пом, тесты

---
+++ src/test/java/RunTest.java
@@ -0,0 +1,26 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RunTest {
+    @Before
+    public void doBefore(){
+        System.out.println("До теста");
+    }
+    @Test
+    public void firstTest(){
+        assertEquals("1"," ");
+        System.out.println("1 test");
+    }
+    @Test
+    public void secondTest(){
+        assertEquals("1","1");
+        System.out.println("2 test");
+    }
+    @After
+    public void doAfter(){
+        System.out.println("После теста");
+    }
+}
