f39c3f64f6622250854d70d9a675504de3da7391
Anton Hromov <svsruof@gmail.com>
2018-12-30 15:41:42 +0300

    Тест для Тинькоф банка

--- src/test/java/RunSeleniumTest.java
+++ src/test/java/RunSeleniumTest.java
@@ -5,12 +5,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 public class RunSeleniumTest {
     private WebDriver webDriver;
@@ -25,12 +24,25 @@ public class RunSeleniumTest {
     }
 
     @Test
-    public void testYandexMarket() throws InterruptedException {
-        Actions actions = new Actions(webDriver);
+    public void testYandexMarket() {
         webDriver.get("https://www.yandex.ru/");
         webDriver.findElement(By.linkText("Маркет")).click();
         webDriver.findElement(By.cssSelector("[data-bem *= Компьютерная]")).click();
         webDriver.findElement(By.cssSelector("a[href *= noutbuki]")).click();
+    }
+
+    @Test
+    public void testTinkof() throws InterruptedException {
+        webDriver.get("https://www.tinkoff.ru/");
+        List<WebElement> webElements = webDriver.findElements(By.xpath("//*[@class='header__1AlOP']"));
+        System.out.println(webElements.get(webElements.size()-1).getTagName());//Обратились к последнему
+        System.out.println(webElements.get(1).getText());//Обратились ко второму
+        System.out.println(webElements.get(1).findElement(By.xpath("..")).getAttribute("class"));//Обратились к родителю второго
+        List<WebElement> parentHrefs = webElements.get(1).findElements(By.xpath("..//a"));
+        System.out.println("------------");
+        for(WebElement we: parentHrefs){
+            System.out.println(we.getText());
+        }
     }
     @After
     public void stop(){
